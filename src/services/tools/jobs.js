b8378d27410b03f0f94eeb5a3dc0bc6d04e77657
Zach <zacharycoss@gmail.com>
2020-10-17 11:28:29 -0500

    updating producerqueue

--- src/services/tools/jobs.js
+++ src/services/tools/jobs.js
@@ -1,21 +1,37 @@
 const 
-    cron = require('cron');
+    cron = require('cron'),
+    CronJob = cron.CronJob,
+    ProducerQueue = require('./producerQueue');
 
 module.exports.init = () => {
     console.log('initializing jobs')
 
-    const CronJob = cron.CronJob
-    // const job = new CronJob('0 */1 * * * *', function() {
-    //     console.log('You will see this message every minute')
-    // })
+    // Queue Jobs
+    const queueStandardOld = new CronJob('0 */5 * * * *', function() {
+        console.log('You will see this message every five minutes')
+        ProducerQueue.findQueueDocs('old')
+    })
+    const queueStandardLate = new CronJob('0 */1 * * * *', function() {
+        console.log('You will see this message every minute')
+        ProducerQueue.findQueueDocs('late')
+    })
+    const queueStandardCurrent = new CronJob('*/20 * * * * *', function() {
+        console.log('You will see this message every twenty seconds')
+        ProducerQueue.findQueueDocs('current')
+    })
+
+    queueStandardOld.start()
+    queueStandardLate.start()
+    queueStandardCurrent.start()
+
     // const job = new CronJob('*/30 * * * * *', function() {
     //     console.log('You will see this message every thirty seconds')
     // })
-    const job = new CronJob('*/10 * * * * *', function() {
-        console.log('You will see this message every ten seconds')
-    })
+    // const job = new CronJob('*/10 * * * * *', function() {
+    //     console.log('You will see this message every ten seconds')
+    // })
     // const job = new CronJob('* * * * * *', function() {
     //     console.log('You will see this message every second')
     // })
-    job.start()
+    
 }
