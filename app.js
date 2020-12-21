c42880961c90119c18d43a8bd94e3c690b2634ee
Zach <zacharycoss@gmail.com>
2020-12-21 15:25:58 -0600

    updating cors

--- app.js
+++ app.js
@@ -20,7 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000','https://api.requestworkbox.com'],
+    methods: ['GET','POST'],
+    allowedHeaders: ['x-api-key','Authorization'],
+    exposedHeaders: [],
+    credentials: true,
+    maxAge: 86400,
+}))
 
 const jwt = require('./src/shared/plugins/network/jwt')
 const router = require('./src/shared/plugins/network/router')
